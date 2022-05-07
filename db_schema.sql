a83afc0c282b9b22538e710aacbbb39388a77389
jugal09xx <65349424+jugal09xx@users.noreply.github.com>
2022-05-07 14:54:35 +0530

    add database schema

---
+++ db_schema.sql
@@ -0,0 +1,45 @@
+CREATE TABLE users(
+userid NUMERIC PRIMARY KEY,
+username VARCHAR(255) NOT NULL UNIQUE,
+pass VARCHAR(255) NOT NULL,
+no_of_followers NUMERIC DEFAULT 0
+);
+
+CREATE TABLE follow(
+userid NUMERIC,
+fname VARCHAR(255),
+FOREIGN KEY (userid) REFERENCES users(userid),
+FOREIGN KEY (fname) REFERENCES users(username)
+);
+
+CREATE TABLE posts(
+userid NUMERIC,
+postid NUMERIC PRIMARY KEY,
+title VARCHAR(255),
+des TEXT,
+FOREIGN KEY (userid) REFERENCES users(userid)
+);
+
+CREATE TABLE comments(
+  userid NUMERIc,
+  postid NUMERIC,
+  content TEXT,
+  comment_date DATE,
+  FOREIGN KEY (userid) REFERENCES users(userid),
+  FOREIGN KEY (postid) REFERENCES posts(postid)
+);
+
+CREATE TABLE likes(
+  userid NUMERIc,
+  postid NUMERIC,
+  isLiked BOOLEAN,
+  FOREIGN KEY (userid) REFERENCES users(userid),
+  FOREIGN KEY (postid) REFERENCES posts(postid)
+)
+
+INSERT INTO users VALUES(1101, 'jugal.lad', '1234hello', 150);
+INSERT INTO users VALUES(1101, 'harsh.pala', 'abcdef', 130);
+INSERT INTO users VALUES(1101, 'devendra.bansal', 'xyzpqr', 110);
+INSERT INTO users VALUES(1101, 'rachit.agarwal', 'helloworld123', 190);
+INSERT INTO users VALUES(1101, 'tanmay.arora', 'welcome@345', 250);
+
